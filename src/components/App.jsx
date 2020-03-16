48534d6a53be10d5fff1241a0e8b60fcbac934c7
Jemal Tahir <jemalmtahir@gmail.com>
2020-03-16 17:51:23 +0200

    added more notes
    dynamically render notes multiple

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./footer";
 import Note from "./Note";
+import notes from "../notes";
+
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(note => {
+        return (
+          <Note key={note.key} titel={note.title} content={note.content} />
+        );
+      })}
       <Footer />
     </div>
   );
